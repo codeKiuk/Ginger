ec49cb0931e2889352b4e903591e6cb70636ff7e
kiuk <ugiya0628@icloud.com>
2021-08-17 14:48:51 +0900

    Route 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import { store } from './redux/store';
-import { Test } from './pages/Test';
+import { withAuth } from './hoc/withAuth';
+import { Home } from './pages/Home'
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { MyContents } from './pages/MyContents';
+import { MyComments } from './pages/MyComments';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
-          <Route path='/' component={Test} />
-
+          <Route path='/' component={withAuth(Home, -1)} />
+          <Route path='/login' component={withAuth(Login, 0)} />
+          <Route path='/register' component={withAuth(Register, 0)} />
+          <Route path='/me/contents' component={withAuth(MyContents, 1)} />
+          <Route path='/me/comments' component={withAuth(MyComments, 1)} />
         </Switch>
       </BrowserRouter>
     </Provider>
