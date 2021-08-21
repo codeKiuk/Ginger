57e21caeb313d9cb6308ebd2f88fd78118a371fa
kiuk <ugiya0628@icloud.com>
2021-08-21 14:54:54 +0900

    가입 후 자동 로그인 로직 추가

--- server/routes/auth/register.js
+++ server/routes/auth/register.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const { User } = require('../../models/User');
 router.post('/api/auth/register', function (req, res) {
 
     const user = new User(req.body);
 
+    User.find({ userID: user.userID }, (err, serchedUser) => {
+        if (err) {
+            console.log('isDuplicated === false, err: ', err);
+        } else {
+            return res.json({ success: false, isDuplicated: true })
+        }
+
+    })
+
     user.save((err, createdUser) => {
         if (err) return res.json({ success: false, err })
 
-        return res.status(200).json({ success: true })
+        return res.status(200).json({
+            success: true,
+            userID: createdUser.userID,
+            password: createdUser.password
+        })
     })
 })
 
