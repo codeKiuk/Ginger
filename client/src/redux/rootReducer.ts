0d7aaae912cfe7e9290d344b4d846b82b5f1a30e
kiuk <ugiya0628@icloud.com>
2021-08-22 15:40:53 +0900

    logout 로직 관련 컴포넌트에 추가 및 api get=>post로 수정

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
-import loginReducer from '@redux/modules/login'
-import authReducer from '@redux/modules/auth'
-import registerReducer from '@redux/modules/register';
+import loginReducer from '@redux/modules/auth/login'
+import logoutReducer from '@redux/modules/auth/logout'
+import authReducer from '@redux/modules/auth/auth'
+import registerReducer from '@redux/modules/auth/register';
 
 const rootReducer = combineReducers({
     login: loginReducer,
+    logout: logoutReducer,
     auth: authReducer,
     register: registerReducer,
 })
