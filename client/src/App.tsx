f4965c003b6cdcba4065337eca116627dc810855
kiuk <ugiya0628@icloud.com>
2021-08-22 15:32:26 +0900

    각 게시글 클릭시 보여주는 Content Component 추가 및 라우트에 Content Component 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,16 +9,18 @@ const Home = React.lazy(() => import('@pages/Home'))
 const Login = React.lazy(() => import('@pages/Login'))
 const Register = React.lazy(() => import('@pages/Register'))
 const MyPage = React.lazy(() => import('@pages/MyPage'))
+const Content = React.lazy(() => import('@pages/Content'));
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={withAuth(Home, -1)} />
+          <Route exact path='/home' component={withAuth(Home, -1)} />
+          <Route exact path='/home/:contentID' component={withAuth(Content, -1)} />
           <Route exact path='/login' component={withAuth(Login, 0)} />
           <Route exact path='/register' component={withAuth(Register, 0)} />
-          <Route exact path='/contents/:user' component={withAuth(MyPage, 1)} />
+          <Route exact path='/contents/:userID' component={withAuth(MyPage, 1)} />
         </Switch>
       </BrowserRouter>
     </Provider>
