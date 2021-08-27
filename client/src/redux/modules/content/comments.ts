7a9414505589caaaf4f5f7ad28cd379b606f7d89
kiuk <ugiya0628@icloud.com>
2021-08-27 21:00:21 +0900

    댓글 작성후 댓글 가져오기 (비동기 처리)

--- client/src/redux/modules/content/comments.ts
+++ client/src/redux/modules/content/comments.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios'
-import { createShorthandPropertyAssignment } from 'typescript';
 
 type Comments = {
     loading: Boolean,
