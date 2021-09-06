daf6a0c30ef81852d27c1f52c5db3ee5b6ad2c4c
kiuk <ugiya0628@icloud.com>
2021-09-06 12:30:29 +0900

    custom hooks 디렉토리 및 별칭 경로 설정

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,12 +5,13 @@ module.exports = override(
     addWebpackAlias({
         '@': path.resolve(__dirname, 'src'),
         '@components': path.resolve(__dirname, 'src/components'),
-        '@commonse': path.resolve(__dirname, 'src/commons'),
+        '@commons': path.resolve(__dirname, 'src/components/commons'),
         "@pages": path.resolve(__dirname, 'src/pages'),
         "@src": path.resolve(__dirname, 'src'),
         "@hoc": path.resolve(__dirname, 'src/hoc'),
         "@redux": path.resolve(__dirname, 'src/redux'),
         "@actions": path.resolve(__dirname, 'src/redux/modules'),
         "@dispatch": path.resolve(__dirname, 'src/redux/hooks'),
+        "@hooks": path.resolve(__dirname, 'src/hooks'),
     }),
 );
